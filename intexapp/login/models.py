7f238e21e206b51034ff7cb7a213a04bee6d8cf8
Dallin Jared <66185492+djared85@users.noreply.github.com>
2020-12-09 09:39:02 -0700

    comit

--- intexapp/login/models.py
+++ intexapp/login/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.db.models.aggregates import Min
 from django.db.models.base import ModelStateFieldsCacheDescriptor
+from django.db.models.deletion import SET_NULL
 
 class PersonType(models.Model):
     personTypeID = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=15)
+    type = models.CharField(max_length=100)
 
 class MinorityType(models.Model):
     minorityTypeID = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=15)
+    type = models.CharField(max_length=100)
 
 class Resumes(models.Model):
     resumeID = models.AutoField(primary_key=True)
@@ -23,7 +24,7 @@ class Person(models.Model):
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=14)
+    phone = models.CharField(max_length=30)
     DOB = models.DateField()
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
@@ -46,14 +47,14 @@ class Person(models.Model):
 
 class CategoryType (models.Model):
     categoryTypeID = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=15)
+    type = models.CharField(max_length=30)
 
     def __str__(self):
         return (self.type)
     
 class CompanySize (models.Model):
     companySizeID = models.AutoField(primary_key=True)
-    size = models.CharField(max_length=15)
+    size = models.CharField(max_length=30)
 
     def __str__(self):
         return (self.size)
@@ -77,19 +78,19 @@ class CompanyEmployee(models.Model):
     employeeID = models.AutoField(primary_key=True)
     companyID = models.ForeignKey(Company, on_delete=models.CASCADE)
     personID = models.ForeignKey(Person, on_delete=models.CASCADE)
-    jobTitle = models.CharField(max_length=20)
+    jobTitle = models.CharField(max_length=20, default='Recruiter')
 
 class JobListings(models.Model):
     jobListingID = models.AutoField(primary_key=True)
     companyID = models.ForeignKey(Company, on_delete=models.CASCADE)
     employeeID = models.ForeignKey(CompanyEmployee, on_delete=models.CASCADE)
     categoryTypeID = models.ForeignKey(CategoryType, on_delete=models.CASCADE)
-    jobTitle = models.CharField(max_length=20)
+    jobTitle = models.CharField(max_length=100, default=SET_NULL)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
     ZIP = models.IntegerField()
     jobDescription = models.CharField(max_length=500)
-    status = models.CharField(max_length=15, null=True)
+    status = models.CharField(max_length=50, null=True)
     dateFilled = models.DateField(null=True)
     savedJobListings = models.ManyToManyField(Person, related_name='user_saved_jobs', through='SavedJobs')
     applicationsTable = models.ManyToManyField(Person, related_name='user_job_applications', through='Applications')
@@ -117,7 +118,7 @@ class JobOffers(models.Model):
     applicationID = models.ForeignKey(Applications, on_delete=models.CASCADE)
     resumeID = models.ForeignKey(Resumes, on_delete=models.CASCADE)
     startDate = models.DateField()
-    applicantResponse = models.CharField(max_length=15, null=True)
+    applicantResponse = models.CharField(max_length=50, null=True)
 
 class Skills(models.Model):
     skillID = models.AutoField(primary_key=True)
